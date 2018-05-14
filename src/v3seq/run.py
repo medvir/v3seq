ff12c6566d1f8a9f991ba5fc6c0c36dee45d69a4
ozagordi <osvaldo.zagordi+git@gmail.com>
2018-05-14 12:52:29 +0000

    Run blast in parallel and write haplotypes ordered by frequency

--- src/v3seq/run.py
+++ src/v3seq/run.py
@@ -20,6 +20,11 @@ d2a = {'AG': 'R', 'CT': 'Y', 'AC': 'M', 'GT': 'K', 'CG': 'S', 'AT': 'W',
 cons_file = resource_filename(__name__, 'db/v3cons.faa')
 cont_file = resource_filename(__name__, 'db/pol_sequences.fasta')
 
+try:
+    n_proc = min(os.cpu_count(), 8)
+except NotImplementedError:
+    n_proc = 2
+logging.info('%d cores that will be used', n_proc)
 
 def grouper(n, iterable, fillvalue=None):
     """Group an iteranle n elements at a time, from itertools examples.
@@ -32,7 +37,9 @@ def grouper(n, iterable, fillvalue=None):
 
 
 def msa_2_df(filename):
-    """Take a MSA in fasta format and return a data frame with position, nucleotide, frequencies."""
+    """Take a MSA in fasta format and return:
+       - a data frame with position, nucleotide, frequencies;
+       - a Counter with haplotypes."""
     from collections import Counter
     msa = AlignIO.read(filename, 'fasta')
     m = len(msa)  # rows, number of sequences
@@ -89,7 +96,8 @@ def remove_matching_reads(filename, contaminant_file):
     if not os.path.exists(cont_file + '.bwt'):
         cml = shlex.split('bwa index %s' % cont_file)
         subprocess.call(cml)
-    cml = 'bwa mem -t 2 %s %s | samtools view -f 4 -h - | samtools bam2fq - | seqtk seq -A - > clean_reads.fasta' % (contaminant_file, filename)
+    cml = 'bwa mem -t 2 %s %s | samtools view -f 4 -h - | samtools bam2fq - | seqtk seq -A - > clean_reads.fasta' % \
+        (contaminant_file, filename)
     subprocess.call(cml, shell=True)
     return 'clean_reads.fasta'
 
@@ -105,12 +113,34 @@ def filter_reads(filename, max_n=100000, min_len=129):
 
 def blast_reads(read_group):
     """Use blastx to align reads to V3 consensus."""
-    SeqIO.write(read_group, 'tmp.fasta', 'fasta')
-    cml = "blastx -task blastx-fast -query tmp.fasta -subject %s \
-    -out out.tsv -num_alignments 1 -evalue 1E-4 \
-    -outfmt '6 qseqid sseqid pident qcovs score length mismatch gapopen qstart qend sstart send'" % (cons_file)
+    tot_seqs = SeqIO.write(read_group, 'tmp.fasta', 'fasta')
+
+    max_n = (tot_seqs / n_proc) + 1
+    # We want to split in n_proc processors, so each file has at most
+    # (tot_seqs / n_proc) + 1 reads
+    cml = "awk -v \"MAX_N=%d\" \'BEGIN {n_seq=0;} /^>/ \
+    {if(n_seq %% %d == 0){file=sprintf(\"splitted_clean_%%d.fasta\", n_seq/%d);} \
+    print >> file; n_seq++; next;} { print >> file; }' %s" % (max_n, max_n, max_n, 'tmp.fasta')
     subprocess.call(cml, shell=True)
-    os.remove('tmp.fasta')
+
+    if sys.platform.startswith('linux'):
+        xargs_thread = 0  # means on all available cores, caution
+    elif sys.platform.startswith('darwin'):
+        xargs_thread = n_proc  # darwin xargs does not accept -P 0
+    else:
+        logging.debug('could not detect system platform: runnning on %d cores', n_proc)
+        xargs_thread = n_proc
+
+    cml = 'seq 0 %s | xargs -P %d -I {} blastx -task blastx-fast -subject %s \
+           -query splitted_clean_{}.fasta -out tmp_{}.tsv -num_alignments 1 -evalue 1E-4 \
+           -outfmt \'6 qseqid sseqid pident qcovs score length mismatch gapopen qstart qend sstart send\'' \
+        % (n_proc - 1, xargs_thread, cons_file)
+    logging.debug('running blast in parallel now')
+    subprocess.call(cml, shell=True)
+
+    subprocess.call('rm tmp.fasta splitted_clean_*fasta', shell=True)
+    subprocess.call('cat tmp*.tsv > out.tsv', shell=True)
+    subprocess.call('rm tmp*.tsv', shell=True)
     als = pd.read_table('out.tsv', header=None,
                         names=['qseqid', 'sseqid', 'pident', 'qcovs', 'score', 'length', 'mismatch', 'gapopen',
                                'qstart', 'qend', 'sstart', 'send'])
@@ -123,7 +153,7 @@ def blast_reads(read_group):
     covering.loc[:, 'qseqid'] = value
     return covering.qseqid.tolist()
 
-def main(filein, min_reads=500):
+def main(filein, min_reads=500, n_group=2000):
     """What the main does."""
     assert os.path.exists(filein)
     logging.info('obtain high quality reads')
@@ -134,14 +164,20 @@ def main(filein, min_reads=500):
     no_pol_reads = SeqIO.parse(no_pol, 'fasta')
     covering_reads = set([])
     logging.info('blast reads in batches until enough are found')
-    for i, group in enumerate(grouper(500, no_pol_reads)):
+    total_blasted = 0
+    for i, group in enumerate(grouper(n_group, no_pol_reads)):
         logging.info('blast call %d', i + 1)
         _ = blast_reads(group)
         covering_reads.update(_)
-        logging.info('covering_reads found so far: %d', len(covering_reads))
+        total_blasted += n_group
+        logging.debug('%d covering reads out of %d blasted - %3.2f %%', len(_), n_group, 100 * float(len(_)) / n_group)
+        logging.debug('covering_reads found so far: %d out of %d total - %3.2f %%', len(covering_reads), total_blasted,
+                      100 * float(len(covering_reads)) / total_blasted)
         if len(covering_reads) >= min_reads:
             break
 
+    logging.info('covering_reads used in MSA: %d out of %d blasted (%3.2f %%)', len(covering_reads), total_blasted,
+                 100 * float(len(covering_reads)) / total_blasted)
     cr = [s for s in SeqIO.parse(no_pol, 'fasta') if s.id in covering_reads]
     n1 = SeqIO.write(cr, 'out.fasta', 'fasta')
     logging.info('%d covering reads in forward orientation', n1)
@@ -160,18 +196,22 @@ def main(filein, min_reads=500):
     cons_seq = df_2_ambiguous_sequence(df)
     SeqIO.write([SeqRecord(Seq(cons_seq), id='v3_consensus', description='')], 'v3cons.fasta', 'fasta')
     haps = []
-    hi = 1
-    tot_reads = df.freq.sum()
-    for h, support in haplotypes.items():
+    hi = 1  # counter for haplotypes, used in fasta file
+    accounted_f = 0.0  # keep track of the cumulative accounted frequency
+    tot_reads = sum(haplotypes.values())
+    for h, support in haplotypes.most_common():
         f = round(float(support) / tot_reads, 2)
+        if f < 0.05:
+            break
+        accounted_f += f
         sr = SeqRecord(Seq(h), id='v3_haplotype_%d-support_%3.2f' % (hi, f), description='')
         haps.append(sr)
         hi += 1
+    logging.info('Total frequency of haplotypes below 5%%: %f', 1 - accounted_f)
     SeqIO.write(haps, 'haplotypes.fasta', 'fasta')
     for f in ['high_quality.fastq', 'out.fasta', 'revout.fasta', 'clean_reads.fasta']:
         os.remove(f)
+    logging.info('Haplotypes written to haplotypes.fasta')
 
 if __name__ == '__main__':
-    msa_2_df('msa.fasta')
-
-    #main(sys.argv[1])
+    main(sys.argv[1])
